9f344f032e057855cf577cb11a3e77aa05a6476a
Natalie Perna <nmperna@gmail.com>
2016-09-04 02:14:36 -0400

    Remove unused imports.

--- app/src/main/java/com/natalieperna/cupful/MainActivity.java
+++ app/src/main/java/com/natalieperna/cupful/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.InputType;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -12,7 +11,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
 
