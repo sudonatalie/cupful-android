126a5d883dff94006874354096c1e1f5815a6db3
Natalie Perna <nmperna@gmail.com>
2017-01-17 19:59:18 -0500

    Rename database file. Remove unnecessary dbVersion table.

--- app/src/main/java/com/natalieperna/cupful/core/Config.java
+++ app/src/main/java/com/natalieperna/cupful/core/Config.java
@@ -1,6 +1,6 @@
 package com.natalieperna.cupful.core;
 
 public class Config {
-    public static final String DATABASE_NAME = "database.db";
+    public static final String DATABASE_NAME = "ingredients.db";
     public static final int DATABASE_VERSION = 1;
 }
