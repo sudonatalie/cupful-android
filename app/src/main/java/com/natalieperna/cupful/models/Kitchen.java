1d60d5e66b793175d366e58419c13f38d9c3c406
Natalie Chouinard <chouinard.nm@gmail.com>
2019-07-12 22:22:41 -0700

    Fix UK tablespoons

--- app/src/main/java/com/natalieperna/cupful/models/Kitchen.java
+++ app/src/main/java/com/natalieperna/cupful/models/Kitchen.java
@@ -10,8 +10,8 @@ public class Kitchen {
     public static final Unit<Volume> CUP_US = NonSI.OUNCE_LIQUID_US.times(8);
     public static final Unit<Volume> CUP_UK = NonSI.OUNCE_LIQUID_UK.times(10);
     public static final Unit<Volume> TABLESPOON_US = NonSI.OUNCE_LIQUID_US.divide(2);
-    public static final Unit<Volume> TABLESPOON_UK = NonSI.OUNCE_LIQUID_UK.times(5 / 8);
-    public static final Unit<Volume> TEASPOON_US = NonSI.OUNCE_LIQUID_US.divide(6);
-    public static final Unit<Volume> TEASPOON_UK = NonSI.OUNCE_LIQUID_UK.divide(6);
+    public static final Unit<Volume> TABLESPOON_UK = NonSI.OUNCE_LIQUID_UK.divide(1.6);
+    public static final Unit<Volume> TEASPOON_US = TABLESPOON_US.divide(3);
+    public static final Unit<Volume> TEASPOON_UK = TABLESPOON_UK.divide(3);
     public static final Unit<VolumetricDensity> G_PER_CUP = SI.GRAM.divide(Kitchen.CUP_US).asType(VolumetricDensity.class);
 }
